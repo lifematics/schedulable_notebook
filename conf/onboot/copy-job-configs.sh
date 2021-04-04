dfef4c018708c74b3f22301d709ba64a96dc47fa
Shota Matsumoto <shota.matsumoto@lifematics.co.jp>
2021-04-04 17:31:51 +0900

    Fix copy-job-configs.sh

--- conf/onboot/copy-job-configs.sh
+++ conf/onboot/copy-job-configs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -d /home/jovyan/.jenkins ]; then
+if [ ! -d /home/jovyan/.jenkins/jobs ]; then
   mkdir -p /home/jovyan/.jenkins/jobs
   chown -R jovyan /home/jovyan/.jenkins/jobs
   cp -pfr /tmp/resource/jenkins-configs/* /home/jovyan/.jenkins/jobs/
